e424b916acf30f95b667f282497004b41a61344c
Oleg Titov <oleggtitoff@gmail.com>
2019-12-15 18:43:58 +0200

    Added int array and averageValue calculation.

--- src/main/java/ua/training/Main.java
+++ src/main/java/ua/training/Main.java
@@ -1,8 +1,15 @@
 package ua.training;
 
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] array = new int[]{0, 1, 2, 3, 4, 5, 6, 0, 7, -1, 8, 9, 10, 0};
+
+        double averageValue = IntStream.of(array)
+                .average()
+                .orElse(0);
 
     }
 
